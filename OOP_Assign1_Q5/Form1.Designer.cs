778353ee2d7442c801daf151ccf3da03d326e397
Muhammad <muhammadali46260@gmail.com>
2023-03-08 00:33:06 -0800

    Initial Commit

---
+++ OOP_Assign1_Q5/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿
+namespace OOP_Assign1_Q5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.inputbox1 = new System.Windows.Forms.TextBox();
+            this.inputbox4 = new System.Windows.Forms.TextBox();
+            this.inputbox7 = new System.Windows.Forms.TextBox();
+            this.inputbox8 = new System.Windows.Forms.TextBox();
+            this.inputbox5 = new System.Windows.Forms.TextBox();
+            this.inputbox2 = new System.Windows.Forms.TextBox();
+            this.inputbox9 = new System.Windows.Forms.TextBox();
+            this.inputbox6 = new System.Windows.Forms.TextBox();
+            this.inputbox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.totalBoxesWithNo = new System.Windows.Forms.Label();
+            this.sumofBoxes = new System.Windows.Forms.Label();
+            this.avgofBoxes = new System.Windows.Forms.Label();
+            this.maxofBoxes = new System.Windows.Forms.Label();
+            this.minofBoxes = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputbox1
+            // 
+            this.inputbox1.Location = new System.Drawing.Point(148, 98);
+            this.inputbox1.Multiline = true;
+            this.inputbox1.Name = "inputbox1";
+            this.inputbox1.Size = new System.Drawing.Size(100, 45);
+            this.inputbox1.TabIndex = 0;
+            // 
+            // inputbox4
+            // 
+            this.inputbox4.Location = new System.Drawing.Point(148, 161);
+            this.inputbox4.Multiline = true;
+            this.inputbox4.Name = "inputbox4";
+            this.inputbox4.Size = new System.Drawing.Size(100, 45);
+            this.inputbox4.TabIndex = 1;
+            // 
+            // inputbox7
+            // 
+            this.inputbox7.Location = new System.Drawing.Point(148, 223);
+            this.inputbox7.Multiline = true;
+            this.inputbox7.Name = "inputbox7";
+            this.inputbox7.Size = new System.Drawing.Size(100, 45);
+            this.inputbox7.TabIndex = 2;
+            // 
+            // inputbox8
+            // 
+            this.inputbox8.Location = new System.Drawing.Point(301, 223);
+            this.inputbox8.Multiline = true;
+            this.inputbox8.Name = "inputbox8";
+            this.inputbox8.Size = new System.Drawing.Size(100, 45);
+            this.inputbox8.TabIndex = 5;
+            // 
+            // inputbox5
+            // 
+            this.inputbox5.Location = new System.Drawing.Point(301, 161);
+            this.inputbox5.Multiline = true;
+            this.inputbox5.Name = "inputbox5";
+            this.inputbox5.Size = new System.Drawing.Size(100, 45);
+            this.inputbox5.TabIndex = 4;
+            // 
+            // inputbox2
+            // 
+            this.inputbox2.Location = new System.Drawing.Point(301, 98);
+            this.inputbox2.Multiline = true;
+            this.inputbox2.Name = "inputbox2";
+            this.inputbox2.Size = new System.Drawing.Size(100, 45);
+            this.inputbox2.TabIndex = 3;
+            // 
+            // inputbox9
+            // 
+            this.inputbox9.Location = new System.Drawing.Point(441, 223);
+            this.inputbox9.Multiline = true;
+            this.inputbox9.Name = "inputbox9";
+            this.inputbox9.Size = new System.Drawing.Size(100, 45);
+            this.inputbox9.TabIndex = 8;
+            // 
+            // inputbox6
+            // 
+            this.inputbox6.Location = new System.Drawing.Point(441, 161);
+            this.inputbox6.Multiline = true;
+            this.inputbox6.Name = "inputbox6";
+            this.inputbox6.Size = new System.Drawing.Size(100, 45);
+            this.inputbox6.TabIndex = 7;
+            // 
+            // inputbox3
+            // 
+            this.inputbox3.Location = new System.Drawing.Point(441, 98);
+            this.inputbox3.Multiline = true;
+            this.inputbox3.Name = "inputbox3";
+            this.inputbox3.Size = new System.Drawing.Size(100, 45);
+            this.inputbox3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(650, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 47);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Nirmala UI", 12F);
+            this.button2.Location = new System.Drawing.Point(650, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 47);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(144, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 24);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Total Boxes With a Number :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(144, 353);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 24);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Sum of All Boxes :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(144, 390);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(257, 24);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Average of Boxes with a No. :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(144, 423);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(212, 24);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Maximum of All Boxes : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(144, 457);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(202, 24);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Minimum of All Boxes :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(210, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(283, 54);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Welcome to \r\nNumber System Application";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // totalBoxesWithNo
+            // 
+            this.totalBoxesWithNo.AutoSize = true;
+            this.totalBoxesWithNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.totalBoxesWithNo.Location = new System.Drawing.Point(541, 317);
+            this.totalBoxesWithNo.Name = "totalBoxesWithNo";
+            this.totalBoxesWithNo.Size = new System.Drawing.Size(20, 24);
+            this.totalBoxesWithNo.TabIndex = 22;
+            this.totalBoxesWithNo.Text = "0";
+            // 
+            // sumofBoxes
+            // 
+            this.sumofBoxes.AutoSize = true;
+            this.sumofBoxes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.sumofBoxes.Location = new System.Drawing.Point(541, 353);
+            this.sumofBoxes.Name = "sumofBoxes";
+            this.sumofBoxes.Size = new System.Drawing.Size(20, 24);
+            this.sumofBoxes.TabIndex = 23;
+            this.sumofBoxes.Text = "?";
+            // 
+            // avgofBoxes
+            // 
+            this.avgofBoxes.AutoSize = true;
+            this.avgofBoxes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.avgofBoxes.Location = new System.Drawing.Point(541, 390);
+            this.avgofBoxes.Name = "avgofBoxes";
+            this.avgofBoxes.Size = new System.Drawing.Size(20, 24);
+            this.avgofBoxes.TabIndex = 24;
+            this.avgofBoxes.Text = "?";
+            // 
+            // maxofBoxes
+            // 
+            this.maxofBoxes.AutoSize = true;
+            this.maxofBoxes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.maxofBoxes.Location = new System.Drawing.Point(541, 423);
+            this.maxofBoxes.Name = "maxofBoxes";
+            this.maxofBoxes.Size = new System.Drawing.Size(20, 24);
+            this.maxofBoxes.TabIndex = 25;
+            this.maxofBoxes.Text = "?";
+            // 
+            // minofBoxes
+            // 
+            this.minofBoxes.AutoSize = true;
+            this.minofBoxes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.minofBoxes.Location = new System.Drawing.Point(541, 457);
+            this.minofBoxes.Name = "minofBoxes";
+            this.minofBoxes.Size = new System.Drawing.Size(20, 24);
+            this.minofBoxes.TabIndex = 26;
+            this.minofBoxes.Text = "?";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(595, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(111, 95);
+            this.pictureBox1.TabIndex = 27;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 532);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.minofBoxes);
+            this.Controls.Add(this.maxofBoxes);
+            this.Controls.Add(this.avgofBoxes);
+            this.Controls.Add(this.sumofBoxes);
+            this.Controls.Add(this.totalBoxesWithNo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.inputbox9);
+            this.Controls.Add(this.inputbox6);
+            this.Controls.Add(this.inputbox3);
+            this.Controls.Add(this.inputbox8);
+            this.Controls.Add(this.inputbox5);
+            this.Controls.Add(this.inputbox2);
+            this.Controls.Add(this.inputbox7);
+            this.Controls.Add(this.inputbox4);
+            this.Controls.Add(this.inputbox1);
+            this.Name = "Form1";
+            this.Text = "Number System";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputbox1;
+        private System.Windows.Forms.TextBox inputbox4;
+        private System.Windows.Forms.TextBox inputbox7;
+        private System.Windows.Forms.TextBox inputbox8;
+        private System.Windows.Forms.TextBox inputbox5;
+        private System.Windows.Forms.TextBox inputbox2;
+        private System.Windows.Forms.TextBox inputbox9;
+        private System.Windows.Forms.TextBox inputbox6;
+        private System.Windows.Forms.TextBox inputbox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label totalBoxesWithNo;
+        private System.Windows.Forms.Label sumofBoxes;
+        private System.Windows.Forms.Label avgofBoxes;
+        private System.Windows.Forms.Label maxofBoxes;
+        private System.Windows.Forms.Label minofBoxes;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ OOP_Assign1_Q5/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Assign1_Q5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            inputbox1.Text = "";
+            inputbox2.Text = "";
+            inputbox3.Text = "";
+            inputbox4.Text = "";
+            inputbox5.Text = "";
+            inputbox6.Text = "";
+            inputbox7.Text = "";
+            inputbox8.Text = "";
+            inputbox9.Text = "";
+            totalBoxesWithNo.Text = Convert.ToString(0);
+            sumofBoxes.Text = "?";
+            avgofBoxes.Text = "?";
+            maxofBoxes.Text = "?";
+            minofBoxes.Text = "?";
+            
+
+
+        }
+
+
+
+        public float Sum()
+        {
+            float total;
+
+            total = float.Parse(inputbox1.Text) + float.Parse(inputbox2.Text) + float.Parse(inputbox3.Text) + float.Parse(inputbox4.Text) + float.Parse(inputbox5.Text) + float.Parse(inputbox6.Text) + float.Parse(inputbox7.Text) + float.Parse(inputbox8.Text) + float.Parse(inputbox9.Text);
+            return total;
+        }
+        public float Avg()
+        {
+
+            float avg;
+            avg = (float.Parse(inputbox1.Text) + float.Parse(inputbox2.Text) + float.Parse(inputbox3.Text) + float.Parse(inputbox4.Text) + float.Parse(inputbox5.Text) + float.Parse(inputbox6.Text) + float.Parse(inputbox7.Text) + float.Parse(inputbox8.Text) + float.Parse(inputbox9.Text)) / 9;
+            return avg;
+
+        }
+
+        public int totalBox()
+        {
+            int no=0;
+            
+
+            if(inputbox1.Text != null)
+                no++;
+            if (inputbox2.Text != null)
+                no++;
+            if (inputbox3.Text != null)
+                no++;
+            if (inputbox4.Text != null)
+                no++;
+            if (inputbox5.Text != null)
+                no++;
+            if (inputbox6.Text != null)
+                no++;
+            if (inputbox7.Text != null)
+                no++;
+            if (inputbox8.Text != null)
+                no++;
+            if (inputbox9.Text != null)
+                no++;
+            return no;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+                sumofBoxes.Text = Convert.ToString(Sum());
+                avgofBoxes.Text = Convert.ToString(Avg());
+
+                float n1, n2, n3, n4, n5, n6, n7, n8, n9, max, min;
+                n1 = float.Parse(inputbox1.Text);
+                n2 = float.Parse(inputbox2.Text);
+                n3 = float.Parse(inputbox3.Text);
+                n4 = float.Parse(inputbox4.Text);
+                n5 = float.Parse(inputbox5.Text);
+                n6 = float.Parse(inputbox6.Text);
+                n7 = float.Parse(inputbox7.Text);
+                n8 = float.Parse(inputbox8.Text);
+                n9 = float.Parse(inputbox9.Text);
+
+                max = n1;
+                min = n1;
+
+                if (n2 < min)
+                    min = n2;
+                if (n2 > max)
+                    max = n2;
+
+                if (n3 < min)
+                    min = n3;
+                if (n3 > max)
+                    max = n3;
+
+                if (n4 < min)
+                    min = n4;
+                if (n4 > max)
+                    max = n4;
+
+                if (n5 < min)
+                    min = n5;
+                if (n5 > max)
+                    max = n5;
+
+                if (n6 < min)
+                    min = n6;
+                if (n6 > max)
+                    max = n6;
+
+                if (n7 < min)
+                    min = n7;
+                if (n7 > max)
+                    max = n7;
+
+                if (n8 < min)
+                    min = n8;
+                if (n8 > max)
+                    max = n8;
+
+                if (n9 < min)
+                    min = n9;
+                if (n9 > max)
+                    max = n9;
+
+                maxofBoxes.Text = max.ToString();
+                minofBoxes.Text = min.ToString();
+
+                totalBoxesWithNo.Text = Convert.ToString(totalBox());
+            
+
+           
+
+
+        }
+    }
+}
